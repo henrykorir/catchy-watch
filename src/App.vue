9f017f97b2002120f77ff73bbf78988cf19efa50
Henry Korir <longkhery@gmail.com>
2025-08-31 06:03:21 +0000

    feat(App-component): added imports to the main component so that all the logic works

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import OnBoardingView from './components/OnBoardingView.vue';
-import HomeView from './components/HomeView.vue';
-import RecommendationView from './components/RecommendationView.vue';
-import AccountView from './components/AccountView.vue';
-import NotFound from './components/NotFound.vue';
+import { ref, computed } from 'vue'
+import OnBoardingView from './components/OnBoardingView.vue'
+import HomeView from './components/HomeView.vue'
+import RecommendationView from './components/RecommendationView.vue'
+import AccountView from './components/AccountView.vue'
+import NotFound from './components/NotFound.vue'
+import MovieDetailsView from './components/MovieDetailsView.vue'
+
+// Static routes
+const routes: Record<string, any> = {
+  '/': OnBoardingView,
+  '/home': HomeView,
+  '/recommendation': RecommendationView,
+  '/account': AccountView,
+}
 
 const currentPath = ref<string>(window.location.hash.slice(1) || '/')
 
 window.addEventListener('hashchange', () => {
   currentPath.value = window.location.hash.slice(1) || '/'
 })
+
+// Match route (supports dynamic `/movie/:id`)
+const currentView = computed(() => {
+  // Dynamic route check
+  if (currentPath.value.startsWith('/movie/')) {
+    const id = currentPath.value.split('/')[2]
+    return { component: MovieDetailsView, props: { id } }
+  }
+
+  // Static route
+  if (routes[currentPath.value]) {
+    return { component: routes[currentPath.value], props: {} }
+  }
+
+  // Fallback
+  return { component: NotFound, props: {} }
+})
 </script>
 
 <template>
-  <OnBoardingView v-if="currentPath === '/'" />
-  <RecommendationView v-else-if="currentPath === '/recommendation'" />
-  <AccountView v-else-if="currentPath === '/account'" />
-  <HomeView v-else-if="currentPath === '/home'" />
-  <NotFound v-else />
+  <component :is="currentView.component" v-bind="currentView.props" />
 </template>
